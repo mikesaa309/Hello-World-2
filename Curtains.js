0b662464f6762f8996fd821c8e9fe2ce8c53046a
Phil Dawson <phildawson@gmail.com>
2022-12-27 01:43:09 +0000

    Initial changes

--- Curtains.js
+++ Curtains.js
@@ -9,75 +9,74 @@ const signTerm = crypto.createHmac('sha256', secret)
     .update(Buffer.from(data, 'utf-8'))
     .digest();
 const sign = signTerm.toString("base64");
-console.log(sign);
+const deviceId = "CE81EAD16115";
 
-const body = JSON.stringify({
+const getStatusPath = `/v1.1/devices/${deviceId}/status`;
+const postCommandPath = `/v1.1/devices/${deviceId}/commands`;
+
+const bodyTurnOff = JSON.stringify({
     "command": "turnOff",
     "parameter": "default",
     "commandType": "command"
 });
 
-const deviceId = "CE81EAD16115";
-const optionsGet = {
-    hostname: 'api.switch-bot.com',
-    port: 443,
-    path: `/v1.1/devices/${deviceId}/status`,
-    method: 'GET',
-    headers: {
-        "Authorization": token,
-        "sign": sign,
-        "nonce": nonce,
-        "t": t,
-        'Content-Type': 'application/json',
-        'Content-Length': body.length,
-    },    
-};
-const optionsPost = {
-    hostname: 'api.switch-bot.com',
-    port: 443,
-    path: `/v1.1/devices/${deviceId}/commands`,
-    method: 'POST',
-    headers: {
-        "Authorization": token,
-        "sign": sign,
-        "nonce": nonce,
-        "t": t,
-        'Content-Type': 'application/json',
-        'Content-Length': body.length,
-    },    
-};
-
-
-const req = https.request(optionsGet, optionsPOST ,res => {
-	let data = '';
-    res.on('data', (chunk) => {
-        data += chunk;
-    });
-
-	res.on('end', () => {
-        let curtainPosition = (JSON.parse(data).body.slidePosition)
-        console.log(curtainPosition)
-        if (curtainPosition > 90)  {
-            JSON.stringify({
-                "command": "turnOff",
-                "parameter": "default",
-                "commandType": "command"
-            });
-        } else {
-            JSON.stringify({
-                "command": "turnOn",
-                "parameter": "default",
-                "commandType": "command"
-            });
-        }
-        })
-    });
-
-
-req.on('error', error => {
-    console.error(error);
+const bodyTurnOn = JSON.stringify({
+    "command": "turnOn",
+    "parameter": "default",
+    "commandType": "command"
 });
 
-req.write(body);
-req.end();
+// console.log(sign);
 
+function apiCall(path, method, body, deviceId) {
+    return new Promise((resolve, reject) => {
+
+        if (body === undefined) {
+            body = '';
+        }
+
+        const options = {
+            Hostname: 'api.switch-bot.com',
+            port: 443,
+            path: path,
+            method: method,
+            headers: {
+                "Authorization": token,
+                "sign": sign,
+                "nonce": nonce,
+                "t": t,
+                'Content-Type': 'application/json',
+                'Content-Length': body.length,
+            },
+        }
+
+        const req = https.request(options, res => {
+            let apiData = '';
+
+            res.on('data', (chunk) => {
+                apiData += chunk;
+            });
+    
+            res.on('end', () => {
+                // let curtainPosition = (JSON.parse(apiData).body.slidePosition)
+                resolve(apiData);
+            })
+        });
+    
+        req.on('error', error => {
+            reject(error);
+        });
+        req.end();
+    })
+   
+}
+
+async function areCurtainsOpen() {
+    return await apiCall(getStatusPath, 'GET')
+}
+
+function toggleCurtains() {
+    console.log(areCurtainsOpen());
+}
+
+toggleCurtains();
